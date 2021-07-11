307e7d77bf3d127852690b08cc64ef89c6a175cc
diana-dagadu <diana.dagadu@amalitech.org>
2021-07-11 04:09:38 +0000

    created a js file to add behaviour

---
+++ script.js
@@ -0,0 +1,32 @@
+const countdown = () => {
+    const launchDate = new Date("August 21, 2021 00:00:00").getTime()
+    const now = new Date().getTime()
+
+    // console.log(launchDate);
+
+    const difference = launchDate - now
+
+    const seconds = 1000
+    const minutes = seconds * 60
+    const hours = minutes * 60
+    const days = hours * 24
+
+    let timeDays = Math.floor(difference / days)
+    let timeHours = Math.floor(difference % days / hours)
+    let timeMinutes = Math.floor(difference % hours / minutes)
+    let timeSeconds = Math.floor(difference % minutes / seconds)
+
+    timeHours = timeHours < 10 ? "0" + timeHours : timeHours
+    timeMinutes = timeMinutes < 10 ? "0" + timeMinutes : timeMinutes
+    timeSeconds = timeSeconds < 10 ? "0" + timeSeconds : timeSeconds
+
+    document.getElementById("days").innerHTML = timeDays
+    document.getElementById("hours").innerHTML = timeHours
+    document.getElementById("minutes").innerHTML = timeMinutes
+    document.getElementById("seconds").innerHTML = timeSeconds
+
+}
+
+setInterval(countdown, 1000)
+
+// countdown()
